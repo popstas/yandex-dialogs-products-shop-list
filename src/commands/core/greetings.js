1cfd1dd746b836c6e5b5153d8d493d57d73798ba
Stanislav Popov <popstas@gmail.com>
2019-02-15 13:44:32 +0500

    feat: 1 помощь

--- src/commands/core/greetings.js
+++ src/commands/core/greetings.js
@@ -5,14 +5,14 @@ const matchers = require('../../matchers');
 // команда по умолчанию (справка)
 module.exports = {
   intent: '',
-  matcher: matchers.strings(['', 'привет', 'приветствие', 'здравствуй']),
+  matcher: matchers.strings(['', 'привет', 'приветствие', 'здравствуй', '1 помощь']),
   // matcher: ['привет', 'приветствие'], // TODO: заменить после выхода sdk 2.0.7
 
   async handler(ctx) {
     if (ctx.message != 'ping') ctx.logMessage(`> ${ctx.message} (welcome)`);
     let msg;
     const buttons = ['список покупок', 'авторизация', 'что нового', 'помощь', 'примеры', 'что ты знаешь', 'команды'];
-    if (ctx.user.state.visitor.visits > 1 || ctx.user.state.visit.messages > 1) {
+    if (ctx.message != '1 помощь' && (ctx.user.state.visitor.visits > 1 || ctx.user.state.visit.messages > 1)) {
       // TODO: разные приветствия
       msg =
         'Привет' +
@@ -22,7 +22,7 @@ module.exports = {
       return ctx.reply(msg, buttons);
     } else {
       msg = [
-        'Я умею запоминать что где лежит или вести список покупок.',
+        'Я умею вести список покупок.',
         'Хотите ознакомиться с возможностями на примере?'
       ];
       return ctx.confirm(msg, help.tour.handler, help.help.handler);
